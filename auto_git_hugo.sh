b5e79f518f57b02aad987a440f58fc0b1bf148be
celiagm <celiagm@debian>
2021-01-12 19:55:13 +0100

    modificaciones

--- auto_git_hugo.sh
+++ auto_git_hugo.sh
@@ -4,8 +4,8 @@ git commit -am "modificaciones"
 git push
 hugo -D
 cd public
-cp -r * /home/celiagm/github/app_static/hugo_netlify_site/public /home/celiagm/github/app_static_hugo/unbitdeinformacioncadadia
-cd /home/celiagm/github/app_static_hugo/unbitdeinformacioncadadia
+cp -r * /home/celiagm/github/app_static/hugo_netlify_site/public /home/celiagm/github/app_static/unbitdeinformacioncadadia
+cd /home/celiagm/github/app_static/unbitdeinformacioncadadia
 git add *
 git commit -m "commit automatico"
 git pull
